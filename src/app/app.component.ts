ca572332293190ea0d6136c5da0e67c0a0742c0b
ali yousuf <ali yousuf>
2017-07-26 11:21:55 +0400

    Refactor to simplify

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,10 +16,10 @@ export class AppComponent {
 
   constructor(private fb: FormBuilder) {
 
-    this.rForm = fb.group({
-      'name': [null, Validators.required],
-      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
-      'validate' : ''
+    this.rForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])),
+      validate: new FormControl('')
     });
 
   }
